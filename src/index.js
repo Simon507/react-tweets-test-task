2304da081c460b8b5edfa777858f298cf7b7aef0
Simon507 <Evgeny.klysak@gmail.com>
2023-04-25 22:33:11 +0300

    deploy 8

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import 'modern-normalize';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter >
+    <BrowserRouter basename="/react-tweets-test-task">
       <App />
     </BrowserRouter>
   </React.StrictMode>
